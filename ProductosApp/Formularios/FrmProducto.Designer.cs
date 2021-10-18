83a2c5b12b18a70c8bb898b7c7f13b620d969fee
Margie Fuertes <margie.fuertes57u@std.uni.edu.ni>
2021-10-18 09:07:54 -0600

    Agregar archivos de proyecto.

---
+++ ProductosApp/Formularios/FrmProducto.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ProductosApp.Formularios
+{
+    partial class FrmProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbEspecie = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblValorU = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpRegistro = new System.Windows.Forms.DateTimePicker();
+            this.nudValorU = new System.Windows.Forms.NumericUpDown();
+            this.nudExist = new System.Windows.Forms.NumericUpDown();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValorU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudExist)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbEspecie);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblValorU);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dtpRegistro);
+            this.groupBox1.Controls.Add(this.nudValorU);
+            this.groupBox1.Controls.Add(this.nudExist);
+            this.groupBox1.Location = new System.Drawing.Point(14, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(603, 355);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Registro";
+            // 
+            // cmbEspecie
+            // 
+            this.cmbEspecie.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEspecie.FormattingEnabled = true;
+            this.cmbEspecie.Items.AddRange(new object[] {
+            "Entrada",
+            "Salida"});
+            this.cmbEspecie.Location = new System.Drawing.Point(228, 137);
+            this.cmbEspecie.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbEspecie.Name = "cmbEspecie";
+            this.cmbEspecie.Size = new System.Drawing.Size(358, 28);
+            this.cmbEspecie.TabIndex = 12;
+            this.cmbEspecie.SelectedIndexChanged += new System.EventHandler(this.cmbEspecie_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Especie";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Fecha de Registro";
+            // 
+            // lblValorU
+            // 
+            this.lblValorU.AutoSize = true;
+            this.lblValorU.Location = new System.Drawing.Point(8, 268);
+            this.lblValorU.Name = "lblValorU";
+            this.lblValorU.Size = new System.Drawing.Size(132, 20);
+            this.lblValorU.TabIndex = 9;
+            this.lblValorU.Text = "Valor por Unidad:";
+            this.lblValorU.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Existencia:";
+            // 
+            // dtpRegistro
+            // 
+            this.dtpRegistro.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpRegistro.Location = new System.Drawing.Point(228, 74);
+            this.dtpRegistro.Name = "dtpRegistro";
+            this.dtpRegistro.Size = new System.Drawing.Size(358, 26);
+            this.dtpRegistro.TabIndex = 4;
+            // 
+            // nudValorU
+            // 
+            this.nudValorU.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudValorU.DecimalPlaces = 2;
+            this.nudValorU.Location = new System.Drawing.Point(228, 265);
+            this.nudValorU.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.nudValorU.Name = "nudValorU";
+            this.nudValorU.Size = new System.Drawing.Size(360, 26);
+            this.nudValorU.TabIndex = 3;
+            // 
+            // nudExist
+            // 
+            this.nudExist.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudExist.Location = new System.Drawing.Point(228, 198);
+            this.nudExist.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudExist.Name = "nudExist";
+            this.nudExist.Size = new System.Drawing.Size(360, 26);
+            this.nudExist.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnCancelar);
+            this.flowLayoutPanel1.Controls.Add(this.btnAceptar);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(91, 392);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(526, 51);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCancelar.Location = new System.Drawing.Point(411, 3);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(112, 37);
+            this.btnCancelar.TabIndex = 0;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnAceptar.Location = new System.Drawing.Point(293, 3);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(112, 37);
+            this.btnAceptar.TabIndex = 1;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // FrmProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 455);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmProducto";
+            this.Text = "FrmProducto";
+            this.Load += new System.EventHandler(this.FrmProducto_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValorU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudExist)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtpRegistro;
+        private System.Windows.Forms.NumericUpDown nudValorU;
+        private System.Windows.Forms.NumericUpDown nudExist;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblValorU;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox cmbEspecie;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ProductosApp/Formularios/FrmProductos.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ProductosApp.Formularios
+{
+    partial class FrmProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.cmbFinderType = new System.Windows.Forms.ComboBox();
+            this.rtbView = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnNuevo);
+            this.flowLayoutPanel1.Controls.Add(this.btnCerrar);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(14, 495);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(873, 76);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.ForeColor = System.Drawing.Color.Green;
+            this.btnNuevo.Location = new System.Drawing.Point(734, 3);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(136, 54);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Agregar nuevo registro";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCerrar.Location = new System.Drawing.Point(550, 5);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(177, 52);
+            this.btnCerrar.TabIndex = 3;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // cmbFinderType
+            // 
+            this.cmbFinderType.AutoCompleteCustomSource.AddRange(new string[] {
+            "Promedio Simple",
+            "PEPS",
+            "UEPS",
+            "Promedio Ponderado"});
+            this.cmbFinderType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFinderType.FormattingEnabled = true;
+            this.cmbFinderType.Items.AddRange(new object[] {
+            "Promedio Simple",
+            "PEPS",
+            "UEPS",
+            "Promedio Ponderado",
+            ""});
+            this.cmbFinderType.Location = new System.Drawing.Point(14, 15);
+            this.cmbFinderType.Name = "cmbFinderType";
+            this.cmbFinderType.Size = new System.Drawing.Size(418, 28);
+            this.cmbFinderType.TabIndex = 1;
+            // 
+            // rtbView
+            // 
+            this.rtbView.Location = new System.Drawing.Point(14, 52);
+            this.rtbView.Name = "rtbView";
+            this.rtbView.ReadOnly = true;
+            this.rtbView.Size = new System.Drawing.Size(872, 437);
+            this.rtbView.TabIndex = 4;
+            this.rtbView.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(723, 17);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(160, 29);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 583);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rtbView);
+            this.Controls.Add(this.cmbFinderType);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "FrmProductos";
+            this.Text = "Catalogo de productos";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.ComboBox cmbFinderType;
+        public System.Windows.Forms.RichTextBox rtbView;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}

56f10fc9188b332d339eb509917c1aa4f650e182
SalgadoSteffania <86125323+SalgadoSteffania@users.noreply.github.com>
2021-10-18 17:39:17 -0600

    Add files via upload

--- ProductosApp/Formularios/FrmMetodos.cs
+++ ProductosApp/Formularios/FrmMetodos.cs
@@ -41,7 +41,7 @@ namespace ProductosApp.Formularios
 		{
 			if(String.IsNullOrWhiteSpace(txtUnidad.Text) && String.IsNullOrWhiteSpace(txtPrecio.Text))
 			{
-				MessageBox.Show("Tienes que llenar todo por favor");
+				MessageBox.Show("No puedes dejar campos vacios");
 				return;
 			}
 
@@ -70,7 +70,7 @@ namespace ProductosApp.Formularios
 			if (char.IsLetter(e.KeyChar))
 			{
 				e.Handled = true;
-				MessageBox.Show("No se pueden letras");
+				MessageBox.Show("No se pueden ingresa letras");
 			}
 		}
 
@@ -88,7 +88,7 @@ namespace ProductosApp.Formularios
 			if (comboBox1.SelectedIndex == 0)
 			{
 				gbVentas.Visible = true;
-				lblLotes.Text = $"Tienes {Model.Largo()} lotes";
+				lblLotes.Text = $"Tienes {Model.Largo()} Unidades";
 				gbCompras.Visible = false;
 					
 			}
@@ -110,21 +110,21 @@ namespace ProductosApp.Formularios
 			if (Char.IsLetter(e.KeyChar))
 			{
 				e.Handled = true;
-				MessageBox.Show("No se pueden letras.");
+				MessageBox.Show("No se pueden ingresar letras.");
 			}
 		}
 
 		private void btnLotes_Click(object sender, EventArgs e)
 		{
-			if (int.Parse(txtLotes.Text) > Model.Largo())
+			if (int.Parse(txtLotes.Text) < Model.Largo())
 			{
-				MessageBox.Show($"No tienes suficientes lotes. tienes {Model.Largo()} lotes");
+				MessageBox.Show($"No tienes suficientes Unidades. tienes {Model.Largo()} ");
 			}
 			else
 			{
 				Model.Vender(int.Parse(txtLotes.Text));
 				rtbProductos.Text += $"(Venta {indiceVenta})  Costo de la venta: {Model.CostoVenta()}, Costo total: {Model.CostoTotal()}{Environment.NewLine}";
-				lblLotes.Text = $"Tienes {Model.Largo()} lotes";
+				lblLotes.Text = $"Tienes {Model.Largo()} Unidades";
 				indiceVenta++;
 			}
 		 }
